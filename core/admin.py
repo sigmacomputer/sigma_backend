93598cb79277cdb0d669edfac25953ef49cb407c
Henish Patel <henishpatel9045@gmail.com>
2024-11-16 11:52:22 +0530

    Add site title

--- core/admin.py
+++ core/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import Site, Gallery, Team, Client, Course
 
 # Register your models here.
+admin.site.site_title = "Sigma Computer Admin"
+admin.site.site_header = "Sigma Computer Admin"
 admin.site.register([Site, Gallery, Team, Client, Course])
